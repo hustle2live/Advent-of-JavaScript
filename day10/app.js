d54ceca47ee77006492c93a00f26b237fb3d6e08
hustle2live <87603877+hustle2live@users.noreply.github.com>
2021-12-29 16:08:36 +0200

    day10 complete

--- day10/app.js
+++ day10/app.js
@@ -1,21 +1,47 @@
-const passwordFields = document.querySelectorAll("input");
+const passwordFields = document.querySelectorAll('input[type="text"]');
 const verifyButton = document.querySelector("button");
 const fields = document.querySelector(".fields");
 
 let userPass = [];
 
 for (let i = 0; i < passwordFields.length; i++) {
-  passwordFields[i].onkeypress = function getKey(event) {
-    userPass[i] = event.key;
-    console.log(userPass);
-    passwordFields[i].value = "*";
-    if (i === passwordFields.length - 1) {
-      verifyButton.focus();
-    } else passwordFields[i + 1].focus();
+  const cell = passwordFields[i];
+  const nextCell = passwordFields[i + 1];
+
+  cell.oninput = () => {
+    if (cell.value.length >= cell.maxLength) {
+      if (i === passwordFields.length - 1) verifyButton.focus();
+      else nextCell.focus();
+    }
   };
+
+  cell.onpaste = function pastePassword(data) {
+    const paste = (data.clipboardData || window.clipboardData).getData("text");
+    let k = 0;
+
+    while (i < passwordFields.length) {
+      passwordFields[i].value = "";
+      passwordFields[i].value = paste.slice(k, k + 1);
+
+      if (i === passwordFields.length - 1) {
+        verifyButton.focus();
+      } else passwordFields[i + 1].focus();
+
+      k++;
+      i++;
+    }
+  };
+
+  cell.classList.add("pincode");
 }
 
-verifyButton.onclick = function passwordVerify() {
-  if (userPass.join("") === "2819") alert("Welcome");
+function passwordVerify(password) {
+  userPass = [];
+  for (let i = 0; i < passwordFields.length; i++)
+    userPass[i] = passwordFields[i].value;
+
+  if (userPass.join("") === password) alert("Welcome");
   else alert("Incorrect password!");
-};
+}
+
+verifyButton.addEventListener("click", () => passwordVerify("2819"));
