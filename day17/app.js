4b06e6af72411cfef37b9b1645dc3daf3851cc9b
hustle2live <87603877+hustle2live@users.noreply.github.com>
2022-01-25 15:16:18 +0200

    day 17 js scroll moderate not ready

--- day17/app.js
+++ day17/app.js
@@ -6,32 +6,27 @@ const headers = document.querySelectorAll("main h3");
 const mainPart = document.querySelector("main");
 const content = document.querySelector(".content");
 
+// const hrefCoords = [180, 240, 285, 345, 414, 463, 532, 582, 640];
+
 let bounding = [];
 
 for (let i = 0; i < hrefs.length; i++) {
   hrefs[i].addEventListener("click", function (event) {
     event.preventDefault();
-
     headers[i].scrollIntoView({
       behavior: "smooth",
       block: "start"
     });
-
-    // bounding[i] = hrefs[i].getBoundingClientRect();
-
-    hrefs.forEach((href) => {
-      if (href.parentNode.classList.contains("selected"))
-        href.parentNode.classList.remove("selected");
-    });
+    deleteActiveItem(hrefs);
     this.parentNode.classList.add("selected");
-
-    // doesParagraphOnViewport(isInViewport(hrefs[i]));
-
-    window.addEventListener("scroll", function (e) {
-      console.log(isInViewport(hrefs[i]));
-    });
   });
 }
+
+const array = {};
+
+window.addEventListener("scroll", function () {
+  headers.forEach((item) => isInViewport(item) && console.log(item));
+});
 
 function isInViewport(elem) {
   let bounding = elem.getBoundingClientRect();
@@ -45,22 +40,9 @@ function isInViewport(elem) {
   );
 }
 
-// main.addEventListener("scroll", function (e) {
-//     if (isInViewport(headers[i]))
-//       console.log(
-//         isInViewport(headers[i]) + ", " + headers[i] + " yes, it's works"
-//       );
-//     else {
-//       console.log("falsie");
-//     }
-//   });
-
-// const doesParagraphOnViewport = (elem) => {
-//   window.addEventListener("scroll", function (e, elem) {
-//     // console.log(e);
-//     if (elem) console.log(" yes, it's works");
-//     else {
-//       console.log("falsie");
-//     }
-//   });
-// };
+const deleteActiveItem = () => {
+  hrefs.forEach((href) => {
+    href.parentNode.classList.contains("selected") &&
+      href.parentNode.classList.remove("selected");
+  });
+};
